c6319e78a466e9e83075162564bf002d59fd6699
AlexBeltran31 <alexbeltranrt@gmail.com>
2026-03-13 16:21:53 +0100

    feat: add i18n support ES/EN with locale middleware and language switcher

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LeadController;
+use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LeadController::class, 'index'])->name('home');
+Route::post('/evaluacion', [LeadController::class, 'store'])->name('leads.store');
+Route::get('/lang/{locale}', [LanguageController::class, 'switch'])
+    ->name('lang.switch')
+    ->whereIn('locale', ['es', 'en']);
